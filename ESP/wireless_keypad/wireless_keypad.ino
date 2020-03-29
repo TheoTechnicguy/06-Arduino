ef98bfdb2c755b2e525de2cd45093c5b25d5ace8
Theo <git@licolas.net>
2020-03-29 15:28:36 +0200

    Update wireless_keypad.ino

--- ESP/wireless_keypad/wireless_keypad.ino
+++ ESP/wireless_keypad/wireless_keypad.ino
@@ -7,69 +7,60 @@
 
 // source: https://diyi0t.com/keypad-tutorial-for-arduino-and-esp8266/
 
-//# pins to use
-//#                    K   J   H   G
-const int KP_ROW[] = {15, 13, 12, 14};
-//                    D8  D7  D6  D5
+#include "Keypad.h" // Add Keypad library
 
-//#                   F  E   D
-const int KP_COL[] = {2, 0, 4};
-//                   D4 D3 D2
+//# pins to use
+//#               K   J   H   G
+byte KP_ROW[] = {15, 13, 12, 14};
+//               D8  D7  D6  D5
+
+//#              F  E   D
+byte KP_COL[] = {0, 2, 4};
+//               D4 D3 D2
 
 const byte KY_ROW = 4;
 const byte KY_COL = 3;
 
 char KEYS[KY_ROW][KY_COL] = {
-  {"1", "2", "3"},
-  {"4", "5", "6"},
-  {"7", "8", "9"},
-  {"*", "0", "#"}
+  {'1', '2', '3'},
+  {'4', '5', '6'},
+  {'7', '8', '9'},
+  {'*', '0', '#'}
 };
 
+//Keypad keypad = Keypad(makeKeymap(KEYS), KP_ROW, KP_COL, KY_ROW, KY_COL); //Keypad keypad = Keypad(makeKeymap(KEYS), KP_ROW, KP_COL, KY_ROW, KY_COL);
+
 int btn = 0;
-String key = "";
 
 void setup() {
   // put your setup code here, to run once:
-
-  //  init KP col keyputs
+  Serial.begin(115200);
   for (int i = 0; i < 3; i++) {
     pinMode(KP_COL[i], OUTPUT);
     digitalWrite(KP_COL[i], HIGH);
   }
-
   for (int i = 0; i < 4; i++) {
     pinMode(KP_ROW[i], INPUT);
   }
 
-  Serial.begin(15200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for (int col = 0; col < 3; col++) {
+
+  for (int col = 0; col < 1; col++) {
+    Serial.print("Col: ");
+    Serial.println(col);
     digitalWrite(KP_COL[col], LOW);
 
     for (int row = 0; row < 4; row++) {
       btn = digitalRead(KP_ROW[row]);
-
-      if (btn == LOW) {
-        key = KEYS[row][col];
-
-        btn = digitalRead(KP_ROW[row]);
-
-        while (btn == LOW) {
-          btn = digitalRead(KP_ROW[row]);
-        }
-        delay(200);
-
-        Serial.println(key);
-      }
+      Serial.print(KEYS[row][col]);
+      Serial.print(" : ");
+      Serial.println(btn);
+      delay(500);
+      btn = 0;
     }
+    digitalWrite(KP_COL[col], HIGH);
   }
-
-  digitalWrite(KP_COL[col], HIGH);
-  // --- END get key ---
-}
-}
 }
