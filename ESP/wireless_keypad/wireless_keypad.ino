fe773686a723e354dee439b71b4622ddf7960db3
Theo <git@licolas.net>
2020-03-29 16:25:16 +0200

    Update wireless_keypad.ino
    Co-Authored-By: acfischer42 <acfischer42@users.noreply.github.com>

--- ESP/wireless_keypad/wireless_keypad.ino
+++ ESP/wireless_keypad/wireless_keypad.ino
@@ -7,60 +7,32 @@
 
 // source: https://diyi0t.com/keypad-tutorial-for-arduino-and-esp8266/
 
-#include "Keypad.h" // Add Keypad library
+#include <Keypad.h>
 
-//# pins to use
-//#               K   J   H   G
-byte KP_ROW[] = {15, 13, 12, 14};
-//               D8  D7  D6  D5
-
-//#              F  E   D
-byte KP_COL[] = {0, 2, 4};
-//               D4 D3 D2
-
-const byte KY_ROW = 4;
-const byte KY_COL = 3;
-
-char KEYS[KY_ROW][KY_COL] = {
-  {'1', '2', '3'},
-  {'4', '5', '6'},
-  {'7', '8', '9'},
-  {'*', '0', '#'}
+const byte ROWS = 4; //four rows
+const byte COLS = 3; //four columns
+//define the cymbols on the buttons of the keypads
+char hexaKeys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
 };
+byte rowPins[ROWS] = {D6, D5, D4, D3}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {D2, D1, D0}; //connect to the column pinouts of the keypad
 
-//Keypad keypad = Keypad(makeKeymap(KEYS), KP_ROW, KP_COL, KY_ROW, KY_COL); //Keypad keypad = Keypad(makeKeymap(KEYS), KP_ROW, KP_COL, KY_ROW, KY_COL);
+//initialize an instance of class NewKeypad
+Keypad customKeypad = Keypad( makeKeymap(hexaKeys), rowPins, colPins, ROWS, COLS); 
 
-int btn = 0;
-
-void setup() {
-  // put your setup code here, to run once:
+void setup(){
   Serial.begin(115200);
-  for (int i = 0; i < 3; i++) {
-    pinMode(KP_COL[i], OUTPUT);
-    digitalWrite(KP_COL[i], HIGH);
-  }
-  for (int i = 0; i < 4; i++) {
-    pinMode(KP_ROW[i], INPUT);
-  }
-
+  Serial.println("Hello World");
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
-  for (int col = 0; col < 1; col++) {
-    Serial.print("Col: ");
-    Serial.println(col);
-    digitalWrite(KP_COL[col], LOW);
-
-    for (int row = 0; row < 4; row++) {
-      btn = digitalRead(KP_ROW[row]);
-      Serial.print(KEYS[row][col]);
-      Serial.print(" : ");
-      Serial.println(btn);
-      delay(500);
-      btn = 0;
+  
+void loop(){
+  char customKey = customKeypad.getKey();
+  
+  if (customKey){
+    Serial.println(customKey);
     }
-    digitalWrite(KP_COL[col], HIGH);
-  }
 }
